c181132ff4db0f4ee303834e50a5e8a2fb7c1e45
AKSHAY <70620976+codexakshay@users.noreply.github.com>
2021-01-05 13:10:19 +0530

    Added : Launch Site On Clicking Logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Hello From BridgeLabz';
+  title = 'HelloWorldAJS';
+  imgUrl;
+  url;
+
+  ngOnInit(): void {
+    this.title = "Hello From BridgeLabz"
+    this.imgUrl="../assets/fav.jpg";
+    this.url="https://www.bridgelabz.com";
+  }
+
+  onClick($event){
+    console.log("Save button is clicked!!", $event);
+    window.open(this.url, "_blank");
+  }
 }
