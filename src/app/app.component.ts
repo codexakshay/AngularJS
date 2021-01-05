e7e780d04b6e096edad9a0c4a221c7a2a4b9e50c
AKSHAY <70620976+codexakshay@users.noreply.github.com>
2021-01-05 13:12:51 +0530

    Added : Display Hello Message With User Input Name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Hello From BridgeLabz';
+  title = 'HelloWorldAJS';
+  imgUrl="../assets/fav.jpg";
+  url="https://www.bridgelabz.com";
+  userName: string = "";
+
+  ngOnInit(): void {
+    this.title = "Hello From BridgeLabz"
+  }
+
+  onClick($event){
+    console.log("Save button is clicked!!", $event);
+    window.open(this.url, "_blank");
+  }
 }
