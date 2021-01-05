5be1525e4f69808b5574bb4a129c87aeadcce13f
AKSHAY <70620976+codexakshay@users.noreply.github.com>
2021-01-05 13:07:54 +0530

    Added : Display Logo Using Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Hello From BridgeLabz';
+  title = 'HelloWorldAJS';
+  imgUrl;
+
+  ngOnInit(): void {
+    this.title = "Hello From BridgeLabz"
+    this.imgUrl="../assets/fav.jpg";
+  }
 }
